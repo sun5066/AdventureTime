f7ed534699c5218b168b30eebf32988b5a100c3b
sun5066 <alstjr4434@naver.com>
2021-11-22 00:30:14 +0900

    grid 뷰 방식으로 변경

--- app/src/main/java/github/sun5066/adventuretime/MainActivity.kt
+++ app/src/main/java/github/sun5066/adventuretime/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -15,8 +19,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberImagePainter
 import github.sun5066.adventuretime.ui.theme.AdventureTimeTheme
@@ -25,6 +31,7 @@ class MainActivity : ComponentActivity() {
 
     private val viewModel: MainViewModel by viewModels()
 
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -38,30 +45,45 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@ExperimentalFoundationApi
 @Composable
 fun Greeting(mainViewModel: MainViewModel = viewModel()) {
     val state by mainViewModel.list.collectAsState()
 
-    LazyColumn {
+    LazyVerticalGrid(cells = GridCells.Adaptive(150.dp)) {
         items(items = state, itemContent = {
-            val painter =
-                rememberImagePainter(
-                    data = it.sprite,
-                    builder = {
-                        crossfade(true)
-                        crossfade(100)
-                        placeholder(R.drawable.ic_launcher_foreground)
-                    }
+            ConstraintLayout(modifier = Modifier.background(color = Color.LightGray)) {
+                val (image, text) = createRefs()
+
+                val painter =
+                    rememberImagePainter(
+                        data = it.sprite,
+                        builder = {
+                            crossfade(true)
+                            crossfade(100)
+                            placeholder(R.drawable.ic_launcher_foreground)
+                        }
+                    )
+
+                Image(
+                    painter = painter,
+                    contentDescription = "Forest Image",
+                    modifier = Modifier.size(200.dp).constrainAs(image) {
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                    contentScale = ContentScale.Crop
                 )
 
-            Image(
-                painter = painter,
-                contentDescription = "Forest Image",
-                modifier = Modifier.size(200.dp),
-                contentScale = ContentScale.Crop
-            )
-
-            Text(text = "Hi, ${it.fullName}")
+                Text(text = it.fullName, Modifier.constrainAs(text) {
+                    top.linkTo(image.bottom)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                })
+            }
         })
     }
 }
